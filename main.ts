fdb4a9bf102d665c48d95e92218caa4240b8721f
StringDynamic <62227698+StringDynamic@users.noreply.github.com>
2020-03-18 16:25:22 +0800

    Create main.ts
    pxt-inspireWheel

---
+++ main.ts
@@ -0,0 +1,10 @@
+/**
+ * Provides access to basic micro:bit functionality.
+ */
+//% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
+namespace inspireWheel {
+    export function showNumber(v: number, interval: number = 150): void
+    {
+      basic.showNumber(number);
+    }
+}
